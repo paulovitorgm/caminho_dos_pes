3dee66f5d1a8fb45751d6cb8164792b2f9e53535
paulovitorgm <paulovitorgaspmelo@hotmail.com>
2023-01-27 10:38:23 -0300

    modificacao url

--- caminho_dos_pes/urls.py
+++ caminho_dos_pes/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pacientes/', include('cadastro_de_pacientes.urls')),
-    path('usuarios/', include('usuarios.urls')),
+    path('', include('usuarios.urls')),
     path('financeiro/', include('financeiro.urls')),
     # path('cadastro_pacientes', include('cadastro_de_pacientes.urls')),
 ]
