fb7278ef97e0b71832be119f873891b7063ba182
paulovitorgm <paulovitorgaspmelo@hotmail.com>
2023-01-30 21:03:00 -0300

    comecando o front

--- usuarios/formulario.py
+++ usuarios/formulario.py
@@ -6,8 +6,8 @@ class Formulario_usuario(forms.ModelForm):
     nome = forms.CharField(label='Nome', widget=forms.TextInput(attrs={'placeholder':'Ex: José'}))
     sobrenome = forms.CharField(label='Sobrenome', widget=forms.TextInput(attrs={'placeholder':'Ex: da Silva'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'placeholder':'jose@gmail.com'}))
-    senha = forms.CharField(label='Senha', widget=forms.PasswordInput())
-    senha2 = forms.CharField(label='Confirme a senha', widget=forms.PasswordInput())
+    senha = forms.CharField(label='Senha', widget=forms.PasswordInput(attrs={'placeholder':'*********'}))
+    senha2 = forms.CharField(label='Confirme a senha', widget=forms.PasswordInput(attrs={'placeholder':'*********'}))
 
     class Meta:
         model = Usuario
