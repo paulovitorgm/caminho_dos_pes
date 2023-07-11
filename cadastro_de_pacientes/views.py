324b36230c518f624317e396861e3b0b6cdc5b36
paulovitorgm <paulovitorgaspmelo@hotmail.com>
2023-07-11 19:30:15 -0300

    incluindo decorators de login_required

--- cadastro_de_pacientes/views.py
+++ cadastro_de_pacientes/views.py
@@ -5,106 +5,100 @@ from . formularios import Anamnese
 from . models.anamnese import Anamnese as Cadastrar_anamnese
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def cadastro_de_pacientes(request):
-   if request.user.is_authenticated:
-      formulario = Cadastro_de_paciente()
-      contexto = {'cadastro_de_pacientes' : formulario}
-      
-      if request.method == 'POST':
-         nome_paciente = request.POST['nome_paciente']
-         sobrenome_paciente = request.POST['sobrenome_paciente']
-         telefone = request.POST['telefone']
-         primeiro_atendimento = request.POST['primeiro_atendimento']
-         email = request.POST['email']
-         sexo = request.POST['sexo']
-         if campo_vazio(nome_paciente):
-            messages.error(request, 'O campo do nome não pode ficar em vazio.')
-            return redirect('cadastro')
-         if campo_vazio(sobrenome_paciente):
-            messages.error(request, 'O campo do sobrenome não pode ficar em vazio.')
-            return redirect('cadastro')
-         if campo_vazio(telefone):
-            messages.error(request, 'O campo do telefone não pode ficar em vazio.')
-            return redirect('cadastro')
-         if verifica_se_somente_numeros(telefone):
-            messages.error(request,'O campo de telefone deve conter apenas números. Tente novamente.')
-            return redirect('cadastro')
-         if paciente_existente(telefone):
-            messages.error(request,'Paciente já cadastrado.')
-            return redirect('cadastro')   
-         else:
-            paciente = Cadastro.objects.create(
-               nome_paciente = nome_paciente,
-               sobrenome_paciente = sobrenome_paciente,
-               telefone = telefone,
-               primeiro_atendimento = primeiro_atendimento,
-               email = email,
-               sexo = sexo
-            )
-            paciente.save()
-            paciente_anamnese = Cadastrar_anamnese.objects.create(paciente=paciente)
-            messages.success(request, 'Paciente cadastrado com sucesso.')
-            contexto = {
-               'paciente' : paciente,
-               'anamnese' : paciente_anamnese
-            }
-            return redirect('cadastro')
-
-      return render(request, 'cadastro_de_pacientes.html', contexto)   
-   else:
-      return redirect('login')
-
-
-def busca(request):
-   if request.user.is_authenticated:
-      lista_de_pacientes = Cadastro.objects.order_by('nome_paciente')
-      if 'busca' in request.GET:
-         nome_a_buscar = request.GET['busca']
-         if nome_a_buscar:
-            lista_de_pacientes = lista_de_pacientes.filter(nome_paciente__icontains = nome_a_buscar)
-            contexto = {'busca': lista_de_pacientes}
-            if lista_de_pacientes:
-               messages.success(request, 'Pacientes encontrados:') 
-            else:
-               messages.error(request,'Nenhum paciente encontrado, tente novamente.')
-            return render(request, 'busca.html',contexto)
+   formulario = Cadastro_de_paciente()
+   contexto = {'cadastro_de_pacientes' : formulario}
+   
+   if request.method == 'POST':
+      nome_paciente = request.POST['nome_paciente']
+      sobrenome_paciente = request.POST['sobrenome_paciente']
+      telefone = request.POST['telefone']
+      primeiro_atendimento = request.POST['primeiro_atendimento']
+      email = request.POST['email']
+      sexo = request.POST['sexo']
+      if campo_vazio(nome_paciente):
+         messages.error(request, 'O campo do nome não pode ficar em vazio.')
+         return redirect('cadastro')
+      if campo_vazio(sobrenome_paciente):
+         messages.error(request, 'O campo do sobrenome não pode ficar em vazio.')
+         return redirect('cadastro')
+      if campo_vazio(telefone):
+         messages.error(request, 'O campo do telefone não pode ficar em vazio.')
+         return redirect('cadastro')
+      if verifica_se_somente_numeros(telefone):
+         messages.error(request,'O campo de telefone deve conter apenas números. Tente novamente.')
+         return redirect('cadastro')
+      if paciente_existente(telefone):
+         messages.error(request,'Paciente já cadastrado.')
+         return redirect('cadastro')   
       else:
-          return render(request, 'busca.html')
-   else:
-      return redirect('login')
-
-
-def salvar_anamnese(request, anamnese_id):
-   if request.user.is_authenticated:
-      paciente = Cadastrar_anamnese.objects.get(pk = anamnese_id)
-
-      if request.method == 'POST':
-         paciente.acompanhamento_medico = request.POST['acompanhamento_medico']
-         paciente.especialidade = request.POST['especialidade']
-         paciente.uso_medicacao = request.POST['uso_medicacao']
-         paciente.medicamento_em_uso = request.POST['medicamento_em_uso']
-         paciente.diabetico = request.POST['diabetico']
-         paciente.hepatite = request.POST['hepatite']
-         paciente.hiv = request.POST['hiv']
-         paciente.problemas_circulatorios = request.POST['problemas_circulatorios']
-         paciente.alergico = request.POST['alergico']
-         paciente.alergia_a = request.POST['alergia_a']
-         paciente.teve_cancer = request.POST['teve_cancer']
-         paciente.tipo_cancer = request.POST['tipo_cancer']
-         paciente.gravidez = request.POST['gravidez']
-         paciente.lactante = request.POST['lactante']
-         paciente.hipertensao = request.POST['hipertensao']
-         paciente.hipotensao = request.POST['hipotensao']
-         paciente.observacoes = request.POST['observacoes']
+         paciente = Cadastro.objects.create(
+            nome_paciente = nome_paciente,
+            sobrenome_paciente = sobrenome_paciente,
+            telefone = telefone,
+            primeiro_atendimento = primeiro_atendimento,
+            email = email,
+            sexo = sexo
+         )
          paciente.save()
-         messages.success(request, 'Anamnese salva com sucesso.') 
-         return render(request, 'busca.html')
+         paciente_anamnese = Cadastrar_anamnese.objects.create(paciente=paciente)
+         messages.success(request, 'Paciente cadastrado com sucesso.')
+         contexto = {
+            'paciente' : paciente,
+            'anamnese' : paciente_anamnese
+         }
+         return redirect('cadastro')
+   return render(request, 'cadastro_de_pacientes.html', contexto)   
+
+
+@login_required(login_url='login')
+def busca(request):
+   lista_de_pacientes = Cadastro.objects.order_by('nome_paciente')
+   if 'busca' in request.GET:
+      nome_a_buscar = request.GET['busca']
+      if nome_a_buscar:
+         lista_de_pacientes = lista_de_pacientes.filter(nome_paciente__icontains = nome_a_buscar)
+         contexto = {'busca': lista_de_pacientes}
+         if lista_de_pacientes:
+            messages.success(request, 'Pacientes encontrados:') 
+         else:
+            messages.error(request,'Nenhum paciente encontrado, tente novamente.')
+         return render(request, 'busca.html',contexto)
    else:
-      return redirect('login')
+         return render(request, 'busca.html')
 
 
+@login_required(login_url='login')
+def salvar_anamnese(request, anamnese_id):
+   paciente = Cadastrar_anamnese.objects.get(pk = anamnese_id)
+   if request.method == 'POST':
+      paciente.acompanhamento_medico = request.POST['acompanhamento_medico']
+      paciente.especialidade = request.POST['especialidade']
+      paciente.uso_medicacao = request.POST['uso_medicacao']
+      paciente.medicamento_em_uso = request.POST['medicamento_em_uso']
+      paciente.diabetico = request.POST['diabetico']
+      paciente.hepatite = request.POST['hepatite']
+      paciente.hiv = request.POST['hiv']
+      paciente.problemas_circulatorios = request.POST['problemas_circulatorios']
+      paciente.alergico = request.POST['alergico']
+      paciente.alergia_a = request.POST['alergia_a']
+      paciente.teve_cancer = request.POST['teve_cancer']
+      paciente.tipo_cancer = request.POST['tipo_cancer']
+      paciente.gravidez = request.POST['gravidez']
+      paciente.lactante = request.POST['lactante']
+      paciente.hipertensao = request.POST['hipertensao']
+      paciente.hipotensao = request.POST['hipotensao']
+      paciente.observacoes = request.POST['observacoes']
+      paciente.save()
+      messages.success(request, 'Anamnese salva com sucesso.') 
+      return render(request, 'busca.html')
+
+
+@login_required(login_url='login')
 def anamnese(request, paciente_id):
    anamnese = get_object_or_404(Cadastrar_anamnese, paciente_id = paciente_id)
    paciente = anamnese.paciente
